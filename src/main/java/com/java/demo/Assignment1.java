4a023cc35c1cc64d246b10c4b1037b4554080001
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2024-11-06 09:27:43 +0530

    Core Java Loops and Conditional Statements

--- src/main/java/com/java/demo/Assignment1.java
+++ src/main/java/com/java/demo/Assignment1.java
@@ -2,8 +2,6 @@ package com.java.demo;
 
 public class Assignment1 {
     // BILLING SYSTEM FOR SUPER MART
-    // product1, product2, product3, product4...
-    // customers1, customer2, customer3...
     // Total_bill && customer type
     // IF total_bill > 2000 and customer_type == SILVER - 5%
     // IF total_bill > 4000 and customer_type == GOLD - 10%
