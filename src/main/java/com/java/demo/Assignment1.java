2b6d45ae3b51c96702f7c256b9e829576c5ac092
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2024-11-06 09:29:52 +0530

    Core Java Loops and Conditional Statements

--- src/main/java/com/java/demo/Assignment1.java
+++ src/main/java/com/java/demo/Assignment1.java
@@ -6,5 +6,8 @@ public class Assignment1 {
     // IF total_bill > 2000 and customer_type == SILVER - 5%
     // IF total_bill > 4000 and customer_type == GOLD - 10%
     // IF total_bill > 6000 and customer_type == PLATINUM - 15%
-
+    int total_bill = 2300;
+    String customer_type = "SILVER";
+    // if
+    // else
 }
