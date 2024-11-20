57ad823076cb0ac0ee66c4cbb7affd32ab6989b8
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2024-11-20 10:15:09 +0530

    Java Procedural Programming Concepts Complete

---
+++ src/main/java/com/java/demo/Assignment4.java
@@ -0,0 +1,28 @@
+package com.java.demo;
+
+public class Assignment4 {
+//    String[] users = {"sid", "manjunath", "avnish", "bharath"};
+//    String[][] stations = {
+//            {"delhi", "hyderabad"},
+//            {"mumbai", "delhi"}
+//    };
+    // TICKETING SYSTEM
+    // SHOW BELOW MENU TO USER
+    // ######### WELCOME TO XYZ TICKETING SYSTEM ############
+    // 1. Register username
+    // 2. change password - default password
+    // 3. create ticket
+    //        - enter from station
+    //        - enter to station
+    // 4. cancel ticket
+    // 5. quit
+
+//    SECOND PART
+    // --------------------------------------
+    // only registered users can create ticket
+    //            - Determine if the user is registered by asking for username
+    // only registered users can cancel ticket
+    //            - Determine if the user is registered by asking for username
+    // maintain a valid list of stations
+    // user should be able to create ticket only from valid list of staions.
+}
